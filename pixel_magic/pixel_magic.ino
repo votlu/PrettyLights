c7d8229d465337f62c739585431dd46a7e29cbe9
votlu <votlu@protonmail.com>
2017-06-17 00:33:09 -0400

    Fix naming, uncomment code

--- pixel_magic/pixel_magic.ino
+++ pixel_magic/pixel_magic.ino
@@ -2,45 +2,36 @@
 
 #define PIN 6
 Adafruit_NeoPixel strip;
-uint16_t indexboi;
-bool meme;
+uint16_t index;
+bool dir; // The direction the index will change in
 
 void setup()
 {
-  // put your setup code here, to run once:
   strip = Adafruit_NeoPixel(196, PIN, NEO_GRB + NEO_KHZ800);
   strip.begin();
   strip.show();
-  
-  // green data
-  // red pwr
-  // white gnd
 
-  indexboi = 0;
-  meme = false;
+  index = 0;
+  dir = false;
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
   for (uint16_t i = 0; i < strip.numPixels(); i++)
   {
-    /*if (i == indexboi)
+    if (i == index)
       strip.setPixelColor(i, strip.Color(0xff, 0xff, 0xff));
     else
       strip.setPixelColor(i, strip.Color(20, 20, 20));
-    */
-    strip.setPixelColor(i, strip.Color(100, 100, 50));
-    //strip.show();
   }
 
-  if (indexboi == strip.numPixels() && !meme)
-    meme = true;
+  if (index == strip.numPixels() && !dir)
+    dir = true;
 
-  if (indexboi == 0 && meme)
-    meme = false;
+  if (index == 0 && dir)
+    dir = false;
 
-  meme ? indexboi-- : indexboi++;
+  dir ? index-- : index++;
 
   strip.show();
 }
